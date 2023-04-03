4cd0f3ba976c092418ed097957b0cd73e8bda32c
sakda homhuan <sakda.homhuan@gmail.com>
2023-04-03 16:59:18 +0700

    update home

--- nodejs/www/ma/home/app.js
+++ nodejs/www/ma/home/app.js
@@ -1,3 +1,12 @@
+
+// const searchParams = new URLSearchParams(window.location.search);
+// const id = searchParams.get('id');
+
+// const layers = id.split(',');
+
+// console.log(layers);
+
+
 let map = L.map('map', {
     // center: [18.80, 98.60],
     center: [18.560018266865583, 98.63156318664552],
@@ -186,6 +195,150 @@ var cm_road = L.tileLayer.wms("/geoserver/cm/wms?", {
     iswms: true,
 });
 
+var cm_road = L.tileLayer.wms("/geoserver/cm/wms?", {
+    layers: 'cm:cm_road',
+    format: 'image/png',
+    transparent: true,
+    name: "lyr",
+    iswms: true,
+});
+
+var mk_pipe = L.tileLayer.wms("/geoserver/cm/wms?", {
+    layers: 'cm:mk_pipe',
+    format: 'image/png',
+    transparent: true,
+    maxZoom: 21,
+    name: "lyr",
+    iswms: true,
+});
+
+var mk_plot_MK = L.tileLayer.wms("/geoserver/cm/wms?", {
+    layers: 'cm:mk_plot_MK',
+    format: 'image/png',
+    transparent: true,
+    maxZoom: 21,
+    name: "lyr",
+    iswms: true,
+    zIndex: 2
+});
+
+var mk_pour_point = L.tileLayer.wms("/geoserver/cm/wms?", {
+    layers: 'cm:mk_pour_point',
+    format: 'image/png',
+    transparent: true,
+    maxZoom: 21,
+    name: "lyr",
+    iswms: true,
+    zIndex: 2
+});
+
+var mk_road = L.tileLayer.wms("/geoserver/cm/wms?", {
+    layers: 'cm:mk_road',
+    format: 'image/png',
+    transparent: true,
+    maxZoom: 21,
+    name: "lyr",
+    iswms: true,
+});
+
+var mk_servicearea = L.tileLayer.wms("/geoserver/cm/wms?", {
+    layers: 'cm:mk_servicearea',
+    format: 'image/png',
+    transparent: true,
+    maxZoom: 21,
+    name: "lyr",
+    iswms: true,
+});
+
+var mk_travel = L.tileLayer.wms("/geoserver/cm/wms?", {
+    layers: 'cm:mk_travel',
+    format: 'image/png',
+    transparent: true,
+    maxZoom: 21,
+    name: "lyr",
+    iswms: true,
+});
+
+var mk_stream = L.tileLayer.wms("/geoserver/cm/wms?", {
+    layers: 'cm:mk_stream',
+    format: 'image/png',
+    transparent: true,
+    maxZoom: 21,
+    name: "lyr",
+    iswms: true,
+    zIndex: 2
+});
+
+
+var ma_boundary = L.tileLayer.wms("/geoserver/cm/wms?", {
+    layers: 'cm:ma_boundary',
+    format: 'image/png',
+    transparent: true,
+    maxZoom: 21,
+    name: "lyr",
+    iswms: true,
+});
+
+var ma_irrigation_line = L.tileLayer.wms("/geoserver/cm/wms?", {
+    layers: 'cm:ma_irrigation_line',
+    format: 'image/png',
+    transparent: true,
+    maxZoom: 21,
+    name: "lyr",
+    iswms: true,
+    zIndex: 2
+});
+
+var ma_plot_greenhouse = L.tileLayer.wms("/geoserver/cm/wms?", {
+    layers: 'cm:ma_plot_greenhouse',
+    format: 'image/png',
+    transparent: true,
+    opacity: 0.6,
+    maxZoom: 21,
+    name: "lyr",
+    iswms: true,
+    zIndex: 2
+});
+
+var ma_servicearea = L.tileLayer.wms("/geoserver/cm/wms?", {
+    layers: 'cm:ma_servicearea',
+    format: 'image/png',
+    transparent: true,
+    maxZoom: 21,
+    name: "lyr",
+    iswms: true,
+    zIndex: 1
+});
+
+var ma_tank_point = L.tileLayer.wms("/geoserver/cm/wms?", {
+    layers: 'cm:ma_tank_point',
+    format: 'image/png',
+    transparent: true,
+    maxZoom: 21,
+    name: "lyr",
+    iswms: true,
+    zIndex: 2
+});
+
+var ma_trail = L.tileLayer.wms("/geoserver/cm/wms?", {
+    layers: 'cm:ma_trail',
+    format: 'image/png',
+    transparent: true,
+    maxZoom: 21,
+    name: "lyr",
+    iswms: true,
+    zIndex: 2
+});
+
+var ma_tree = L.tileLayer.wms("/geoserver/cm/wms?", {
+    layers: 'cm:ma_tree',
+    format: 'image/png',
+    transparent: true,
+    maxZoom: 21,
+    name: "lyr",
+    iswms: true,
+});
+
 var cm_village = L.tileLayer.wms("/geoserver/cm/wms?", {
     layers: 'cm:cm_village',
     format: 'image/png',
@@ -195,64 +348,8 @@ var cm_village = L.tileLayer.wms("/geoserver/cm/wms?", {
     // CQL_FILTER: 'pro_code=53'
 });
 
-var ma_boundary = L.tileLayer.wms("/geoserver/cm/wms?", {
-    layers: 'cm:ma_boundary',
-    format: 'image/png',
-    transparent: true,
-    name: "lyr",
-    iswms: true,
-});
-
-var ma_irrigation_line = L.tileLayer.wms("/geoserver/cm/wms?", {
-    layers: 'cm:ma_irrigation_line',
-    format: 'image/png',
-    transparent: true,
-    name: "lyr",
-    iswms: true,
-});
-
-var ma_plot_greenhouse = L.tileLayer.wms("/geoserver/cm/wms?", {
-    layers: 'cm:ma_plot_greenhouse',
-    format: 'image/png',
-    transparent: true,
-    name: "lyr",
-    iswms: true,
-});
-
-var ma_servicearea = L.tileLayer.wms("/geoserver/cm/wms?", {
-    layers: 'cm:ma_servicearea',
-    format: 'image/png',
-    transparent: true,
-    name: "lyr",
-    iswms: true,
-});
-
-var ma_tank_point = L.tileLayer.wms("/geoserver/cm/wms?", {
-    layers: 'cm:ma_tank_point',
-    format: 'image/png',
-    transparent: true,
-    name: "lyr",
-    iswms: true,
-});
-
-var ma_trail = L.tileLayer.wms("/geoserver/cm/wms?", {
-    layers: 'cm:ma_trail',
-    format: 'image/png',
-    transparent: true,
-    name: "lyr",
-    iswms: true,
-});
-
-var ma_tree = L.tileLayer.wms("/geoserver/cm/wms?", {
-    layers: 'cm:ma_tree',
-    format: 'image/png',
-    transparent: true,
-    name: "lyr",
-    iswms: true,
-});
-
 let base = {
-    mapbox: mapbox.addTo(map),
+    mapbox: mapbox,
     ghyb: ghyb,
     grod: grod,
     gter: gter
@@ -270,51 +367,105 @@ $("input[name='basemap']").change(async (r) => {
     base[`${basemap}`].addTo(map);
 })
 
-let lyr = {
-    cm_waterbody,
-    cm_forest_type,
-    cm_tambon,
-    cm_amphoe,
-    cm_province,
-    cm_village,
-    village_bound,
-    planuse,
-    cm_forest_use,
-    cm_geology,
-    cm_fault,
-    cm_contour,
-    cm_agriculture_zoning,
-    cm_river,
-    cm_road,
-    cm_soil,
-    cm_landuse_2543,
-    cm_landuse_2538,
-    ma_boundary,
-    ma_irrigation_line,
-    ma_plot_greenhouse,
-    ma_servicearea,
-    ma_tank_point,
-    ma_trail,
-    ma_tree
+const legUrl = "/geoserver/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&legend_options=fontName:Kanit;fontAntiAliasing:true;dpi:96&LAYER=";
+
+const irr = [{
+    name: "ถังเก็บน้ำ",
+    layer_txt: "ma_tank_point",
+    layer_var: ma_tank_point,
+    legend: legUrl + "cm:ma_tank_point"
+}, {
+    name: "ทางน้ำ",
+    layer_txt: "ma_irrigation_line",
+    layer_var: ma_irrigation_line,
+    legend: legUrl + "cm:ma_irrigation_line"
+}, {
+    name: "โรงเรือน",
+    layer_txt: "ma_plot_greenhouse",
+    layer_var: ma_plot_greenhouse,
+    legend: legUrl + "cm:ma_plot_greenhouse"
+}, {
+    name: "พื้นที่บริการ",
+    layer_txt: "ma_servicearea",
+    layer_var: ma_servicearea,
+    legend: legUrl + "cm:ma_servicearea"
+}, {
+    name: "ขอบเขตหมู่บ้าน",
+    layer_txt: "ma_boundary",
+    layer_var: ma_boundary,
+    legend: legUrl + "cm:ma_boundary"
+}]
+
+const trv = [{
+    name: "ทางเดิน",
+    layer_txt: "ma_trail",
+    layer_var: ma_trail,
+    legend: legUrl + "cm:ma_trail"
+}, {
+    name: "ทางน้ำ",
+    layer_txt: "ma_irrigation_line",
+    layer_var: ma_irrigation_line,
+    legend: legUrl + "cm:ma_irrigation_line"
+}, {
+    name: "ถนน",
+    layer_txt: "cm_road",
+    layer_var: cm_road,
+    legend: legUrl + "cm:cm_road"
+}, {
+    name: "พื้นที่บริการ",
+    layer_txt: "ma_servicearea",
+    layer_var: ma_servicearea,
+    legend: legUrl + "cm:ma_servicearea"
+}, {
+    name: "ขอบเขตหมู่บ้าน",
+    layer_txt: "ma_boundary",
+    layer_var: ma_boundary,
+    legend: legUrl + "cm:ma_boundary"
+}]
+
+let wmsLyr = [];
+let getLayer = () => {
+    wmsLyr = [];
+    map.eachLayer(i => {
+        if (i.options.iswms) {
+            wmsLyr.push(i.options.layers);
+        }
+    })
 }
 
-// set default layer
-cm_province.addTo(map);
-$("#lyr_prov").prop("checked", true);
+const listLayers = (layerOpt, div, layerGroup) => {
+    document.getElementById("layerGroup").innerHTML = layerGroup;
+    document.getElementById("lyrs").innerHTML = "";
+    $("#lyr_prov").prop("checked", true);
+    let lyrArr = [];
+    layerOpt.map(i => {
+        let chk = `<input type="checkbox" id="${i.layer_txt}" name="${i.layer_txt}" value="${i.layer_txt}">`;
+        let lbl = `<label for="${i.layer_txt}">${i.name}</label>`;
+        let leg = `<img src="${i.legend}" alt="">`;
+        let div = `<div class="lyr">${chk} ${lbl}<br>${leg}</div>`;
+        lyrArr.push(i.layer_var);
+        $("#lyrs").append(div);
+    })
 
-cm_amphoe.addTo(map);
-$("#lyr_amp").prop("checked", true);
+    $("#" + div).prop("checked", true);
+    $("#ma_boundary").prop("checked", true);
 
-cm_road.addTo(map);
-$("#lyr_cm_road").prop("checked", true);
+    changeOverlay(lyrArr);
+    changeBasemap();
+}
 
-village_bound.addTo(map);
-$("#lyr_village_bound").prop("checked", true);
+const changeBasemap = async (r) => {
+    await map.eachLayer(i => {
+        if (i.options.name == "base") {
+            map.removeLayer(i)
+        }
+    })
 
-cm_village.addTo(map);
-$("#lyr_cm_village").prop("checked", true);
+    let basemap = $("input[name='basemap']:checked").val();
+    base[`${basemap}`].addTo(map);
+}
 
-$("input[type=checkbox]").change(async () => {
+const changeOverlay = async (lyrArr) => {
     await map.eachLayer(i => {
         if (i.options.name == "lyr") {
             map.removeLayer(i)
@@ -325,42 +476,133 @@ $("input[type=checkbox]").change(async () => {
     await $('input[type=checkbox]:checked').each(function () {
         chk.push($(this).val());
     });
-    // console.log(chk);
-    chk.map(i => {
-        if (lyr[`${i}`]) {
-            lyr[`${i}`].addTo(map);
+
+    chk.forEach(cb => {
+        lyrArr.forEach(lyr => {
+            if (lyr.options.layers == `cm:${cb}`) {
+                lyr.addTo(map);
+            }
+        })
+    })
+
+    getLayer();
+
+    $("input[type=checkbox]").change(async () => {
+        await map.eachLayer(i => {
+            if (i.options.name == "lyr") {
+                map.removeLayer(i)
+            }
+        })
+
+        let chk = [];
+        await $('input[type=checkbox]:checked').each(function () {
+            chk.push($(this).val());
+        });
+
+        chk.forEach(cb => {
+            lyrArr.forEach(lyr => {
+                if (lyr.options.layers == `cm:${cb}`) {
+                    lyr.addTo(map);
+                }
+            })
+        })
+        getLayer();
+    })
+}
+
+listLayers(irr, "ghyb", "ระบบชลประทาน");
+
+$("#btn-irr").click(() => {
+    listLayers(irr, "ghyb", "ระบบชลประทาน");
+})
+
+$("#btn-trv").click(() => {
+    listLayers(trv, "gter", "เส้นทางศึกษาธรรมชาติ");
+})
+
+map.on("click", async (e) => {
+    var pnt = await map.latLngToContainerPoint(e.latlng, map.getZoom());
+    var size = await map.getSize();
+    var bbox = await map.getBounds().toBBoxString();
+    $("#showlat").text(e.latlng.lat)
+    $("#showlng").text(e.latlng.lng)
+
+    let lyrInfoUrl = "/geoserver/wms?SERVICE=WMS" +
+        "&VERSION=1.1.1&REQUEST=GetFeatureInfo" +
+        "&QUERY_LAYERS=" + wmsLyr +
+        "&LAYERS=" + wmsLyr +
+        "&Feature_count=" + wmsLyr.length +
+        "&INFO_FORMAT=application/json" +
+        "&X=" + Math.round(pnt.x) +
+        "&Y=" + Math.round(pnt.y) +
+        "&SRS=EPSG:4326" +
+        "&WIDTH=" + size.x +
+        "&HEIGHT=" + size.y +
+        "&BBOX=" + bbox
+
+    // console.log(lyrInfoUrl);
+
+    axios.get(lyrInfoUrl).then(res => {
+        if (res.data.features.length > 0) {
+            var txt;
+            res.data.features.forEach(i => {
+                console.log(i);
+                txt += `${i.id}:  ${i.properties.tb}<br>`;
+            });
+
+            L.popup()
+                .setLatLng(e.latlng)
+                .setContent(txt)
+                .openOn(map);
+        }
+    }).catch(err => { })
+});
+
+let zoomMap = async (geomTxt) => {
+    await map.eachLayer(i => {
+        if (i.options.name == "query") {
+            map.removeLayer(i)
         }
     })
-})
 
-const legUrl = "/geoserver/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&legend_options=fontName:Kanit;fontAntiAliasing:true;dpi:96&LAYER=";
-$("#proLegend").attr("src", legUrl + "cm:cm_province&rule=Large");
-$("#ampLegend").attr("src", legUrl + "cm:cm_amphoe&rule=Large");
-$("#tamLegend").attr("src", legUrl + "cm:cm_tambon");
-$("#village_boundLegend").attr("src", legUrl + "cm:village_bound");
-$("#cm_waterbodyLegend").attr("src", legUrl + "cm:cm_waterbody");
-$("#planuseLegend").attr("src", legUrl + "cm:planuse");
-$("#villLegend").attr("src", legUrl + "cm:cm_village&rule=Large");
-$("#cm_forest_typeLegend").attr("src", legUrl + "cm:cm_forest_type");
-$("#cm_forest_useLegend").attr("src", legUrl + "cm:cm_forest_use");
-$("#cm_geologyLegend").attr("src", legUrl + "cm:cm_geology");
-$("#cm_faultLegend").attr("src", legUrl + "cm:cm_fault");
-$("#cm_contourLegend").attr("src", legUrl + "cm:cm_contour");
-$("#cm_agriculture_zoningLegend").attr("src", legUrl + "cm:cm_agriculture_zoning");
-$("#cm_riverLegend").attr("src", legUrl + "cm:cm_river");
-$("#cm_roadLegend").attr("src", legUrl + "cm:cm_road");
-$("#cm_soilLegend").attr("src", legUrl + "cm:cm_soil");
-$("#cm_landuse_2543Legend").attr("src", legUrl + "cm:cm_landuse_2543");
-$("#cm_landuse_2538Legend").attr("src", legUrl + "cm:cm_landuse_2538");
+    let geom = JSON.parse(geomTxt);
+    // console.log(geom);
 
-$("#ma_tank_pointLegend").attr("src", legUrl + "cm:ma_tank_point");
-$("#ma_treeLegend").attr("src", legUrl + "cm:ma_tree");
-$("#ma_trailLegend").attr("src", legUrl + "cm:ma_trail");
-$("#ma_irrigation_lineLegend").attr("src", legUrl + "cm:ma_irrigation_line");
-$("#ma_plot_greenhouseLegend").attr("src", legUrl + "cm:ma_plot_greenhouse");
-$("#ma_boundaryLegend").attr("src", legUrl + "cm:ma_boundary");
-$("#ma_serviceareaLegend").attr("src", legUrl + "cm:ma_servicearea");
+    if (geom.type == "Point") {
+        L.circleMarker([geom.coordinates[1], geom.coordinates[0]], { radius: 8, name: 'query' }).addTo(map);
+        map.setView([geom.coordinates[1], geom.coordinates[0]], 18);
+    } else {
+        // console.log(geom.coordinates[0][0]);
+        let g = L.geoJSON(geom, { name: 'query' }).addTo(map);
+        map.fitBounds(g.getBounds());
+    }
+}
 
-map.on("click", (e) => {
-    console.log(e);
-})
+var table = $('#table').DataTable();
+
+const searchWfs = async () => {
+    let text = $("#search").val();
+    let layer = $("#select_layer").val();
+
+    // $('#table').DataTable().de;
+    $('#table').dataTable().fnDestroy();
+
+    table = $('#table').DataTable({
+        ajax: {
+            type: "POST",
+            url: `/api/get_feature`,
+            data: { text, layer },
+            dataSrc: 'data',
+        },
+        columns: [
+            { data: 'tb' }, {
+                data: null,
+                render: function (data, type, row, meta) {
+                    let mBtn = `<button class= "btn btn-success" onclick='zoomMap(${JSON.stringify(row.geom)})'><i class="fas fa-map-marker-alt"></i>&nbsp;ซูมไปยังตำแหน่งนี้</button>`
+                    return `${mBtn}`
+                },
+            }
+        ],
+        searching: false,
+    });
+}
